1db3c65b045841480c88895a1afa9afa7d29297a
ItsSony2003 <sonythaiduong2003@gmail.com>
2024-11-28 19:57:37 +0700

    Create Program.cs

---
+++ TemperatureMethod/TemperatureMethod/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TemperatureMethod
+{
+    internal class Program
+    {
+        public static double CelsiusToFahrenheit(double celsius)
+        {
+            double fahrenheit = (9.0 / 5) * celsius + 32;
+            return fahrenheit;
+        }
+
+        public static double FahrenheitToCelsius(double fahrenheit)
+        {
+            double celsius = (5.0 / 9) * (fahrenheit - 32);
+            return celsius;
+        }
+
+
+
+        static void Main(string[] args)
+        {
+            double fahrenheit;
+            double celsius;
+            int choice;
+
+            do
+            {
+                Console.WriteLine("Menu.");
+                Console.WriteLine("1. Fahrenheit to Celsius");
+                Console.WriteLine("2. Celsius to Fahrenheit");
+                Console.WriteLine("0. Exit");
+                Console.Write("Enter your choice: ");
+                choice = Int32.Parse(Console.ReadLine());
+                Console.WriteLine();
+
+                switch (choice)
+                {
+                    case 1:
+                        Console.Write("Enter fahrenheit: ");
+                        fahrenheit = Double.Parse(Console.ReadLine());
+                        Console.WriteLine("Fahrenheit to Celsius: " + FahrenheitToCelsius(fahrenheit));
+                        Console.WriteLine();
+                        break;
+                    case 2:
+                        Console.Write("Enter Celsius: ");
+                        celsius = Double.Parse(Console.ReadLine());
+                        Console.WriteLine("Celsius to Fahrenheit: " + CelsiusToFahrenheit(celsius));
+                        Console.WriteLine();
+                        break;
+                    case 0:
+                        Environment.Exit(0);
+                        break;
+                    default:
+                        Console.Write("Please Enter The Right Option!");
+                        Console.WriteLine();
+                        break;
+                }
+            } while (choice != 0);
+            Console.ReadKey();
+        }
+    }
+}
